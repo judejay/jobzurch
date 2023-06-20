db0bbb63b54f3123869e3210664dc93ee8fe3636
judejay <judejay26@gmail.com>
2023-06-20 16:23:19 +0100

    removed helper function to check if URL

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-
 import styles from "./nearbyjobcard.style";
-import { checkImageURL } from "../../../../utils";
+//import { checkImageURL } from "../../../../utils";
+import { icons } from "../../../../constants";
 
 const NearbyJobCard = ({ job, handleNavigate}) => {
   return (
@@ -11,11 +11,8 @@ const NearbyJobCard = ({ job, handleNavigate}) => {
     >
       <TouchableOpacity style={styles.logoContainer}>
         <Image
-          source={{
-            uri: checkImageURL(job?.employer_logo)
-              ? job.employer_logo
-              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
-          }}
+          source={icons.j310}
+          
           resizeMode='contain'
           style={styles.logoImage}
         />
--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./PopularJobCard.style";
 import { checkImageURL } from "../../../../utils";
+import { icons } from "../../../../constants";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
@@ -11,11 +12,8 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
-          source={{
-            uri: checkImageURL(item?.employer_logo)
-              ? item.employer_logo
-              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
-          }}
+          source={ icons.j310
+          }
           resizeMode='contain'
           style={styles.logoImage}
         />
